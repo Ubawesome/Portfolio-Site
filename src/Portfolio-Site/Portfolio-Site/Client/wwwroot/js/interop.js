d3b128baf523fc42fa07b9075c3d045889f181ce
Caleb Frasier <calebjfrasier@gmail.com>
2022-11-12 16:59:20 -0800

    Added swipe controls for desktop, drag follow animation

--- src/Portfolio-Site/Portfolio-Site/Client/wwwroot/js/interop.js
+++ src/Portfolio-Site/Portfolio-Site/Client/wwwroot/js/interop.js
@@ -8,6 +8,18 @@
     }
 }
 
+function addElementClass(element, className) {
+    if (element instanceof HTMLElement && (typeof className === 'string' || className instanceof String)) {
+        element.classList.add(className);
+    }
+}
+
+function removeElementClass(element, className) {
+    if (element instanceof HTMLElement && (typeof className === 'string' || className instanceof String)) {
+        element.classList.remove(className);
+    }
+}
+
 function setFocus(element) {
     if (element instanceof HTMLElement) {
         element.focus();
@@ -22,3 +34,52 @@ function scrollToElement(elementId) {
         window.scrollBy(0, elementPos);
     }
 };
+
+function getTransformValues(element) {
+    const transform = window.getComputedStyle(element).getPropertyValue('transform');
+    if (transform != 'none') {
+        const matrix = transform.replace(/[^0-9\-.,]/g, '').split(',');
+
+        return matrix;
+    }
+}
+
+let follow = false;
+
+let toggle;
+
+function navDragStart(element) {
+    follow = true;
+    toggle = element;
+}
+
+function navDrag(element, dragY) {
+    let offset = 0;
+    if (element.classList.contains('active')) {
+        const transformMatrix = getTransformValues(element);
+        const y = transformMatrix[13] || transformMatrix[5]; //translate y
+
+        offset = y;
+    }
+
+    const toggleButton = element.querySelector('.nav-toggle');
+    element.style.setProperty('--toggle-drag-position', (Number(dragY) - Number(offset) - (toggleButton.clientHeight / 2)) + 'px');
+}
+
+function navDragEnd(element) {
+    toggle.style.setProperty('--toggle-drag-position', 0 + 'px');
+}
+
+
+function addToggleSwipeListener(element) {
+    toggle = element;
+
+}
+
+function navSwipe(e) {
+    console.log(e);
+}
+
+//window.addEventListener('onmousemove', navDrag);
+//window.addEventListener('onmouseup', navDragEnd);
+
