e0bfc07c3d82d7d10a162e72ee83ec1042080345
Caleb Frasier <calebjfrasier@gmail.com>
2022-11-22 01:15:54 -0800

    Added InputBase, base styles for inputs, TextInput component

---
+++ src/Portfolio-Site/Portfolio-Site/Client/Components/InputBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public abstract class InputBase : ComponentBase {
+    [Parameter]
+    public string Id { get; set; } = new Guid().ToString();
+
+    [Parameter]
+    public string? Label { get; set; }
+}
