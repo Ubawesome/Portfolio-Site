38d66f3711cad6465d564ed1c7268a826eec1a70
Caleb Frasier <calebjfrasier@gmail.com>
2022-11-23 14:44:19 -0800

    Removed InputBase, using Blazor inputs, styling. Added FormField

--- src/Portfolio-Site/Portfolio-Site/Client/Components/InputBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-public abstract class InputBase : ComponentBase {
-    [Parameter]
-    public string Id { get; set; } = new Guid().ToString();
-
-    [Parameter]
-    public string? Label { get; set; }
-}
