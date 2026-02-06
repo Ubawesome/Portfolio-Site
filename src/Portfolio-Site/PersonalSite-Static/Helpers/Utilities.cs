1f9c5ea54cb7f69b68219e88f7259621782b4f80
Caleb Frasier <calebjfrasier@gmail.com>
2026-02-05 20:24:32 -0800

    Moved project to .net 10 without server or shared projects

---
+++ src/Portfolio-Site/PersonalSite-Static/Helpers/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace PersonalSite_Static.Helpers
+{
+    public static class Utilities
+    {
+        public static string GetDisplayName<T>(Expression<Func<T>> property)
+        {
+            var expression = (MemberExpression)property.Body;
+            var value = expression.Member.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+            return value?.Name ?? expression.Member.Name ?? "";
+        }
+    }
+}
