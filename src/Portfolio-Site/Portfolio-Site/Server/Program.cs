560a4c7847547948624c0902a61ccc32f2134c05
Caleb Frasier <calebjfrasier@gmail.com>
2026-02-05 19:00:42 -0800

    Updated program.cs, removing unneeded services

--- src/Portfolio-Site/Portfolio-Site/Server/Program.cs
+++ src/Portfolio-Site/Portfolio-Site/Server/Program.cs
@@ -4,8 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddCors();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull);
+
+
+builder.Services.AddSignalR(hubOptions =>
+{
+    hubOptions.EnableDetailedErrors = true;
+    hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(10);
+    hubOptions.HandshakeTimeout = TimeSpan.FromSeconds(10);
+});
+
+builder.Services.AddAuthentication();
+
+builder.Logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Debug);
+builder.Logging.AddFilter("Microsoft.ASpNetCore.Http.Connections", LogLevel.Debug);
 
 var app = builder.Build();
 
@@ -22,15 +35,19 @@ else
 }
 
 app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+});
 
+app.UseAuthentication();
+app.UseWebSockets();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 
-app.MapRazorPages();
-app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
